f085b82cbed368ca29cb1a8fce3c60c38672027e
drik98 <hendrikschmitz306@gmail.com>
2020-09-22 16:39:45 +0200

    REPLACE v-ifs with v-shows

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -63,41 +63,41 @@
         :key="row.node.id"
         :icon="'mdi-flag-checkered'"
         :color=" Date.parse(row.node.date) < Date.now() ? 'grey': 'deep-orange'"
-        v-if="Date.parse(row.node.date) >= Date.parse(dateVon) && Date.parse(row.node.date) <= Date.parse(`${dateBis} 23:59:59`) "
+        v-show="Date.parse(row.node.date) >= Date.parse(dateVon) && Date.parse(row.node.date) <= Date.parse(`${dateBis} 23:59:59`) "
       >
         <span slot="opposite">{{row.node.Datum}}</span>
         <v-card class="elevation-2" :disabled="Date.parse(row.node.date) < Date.now()">
           <v-card-title class="headline">{{row.node.Rennen}}</v-card-title>
           <v-card-subtitle>{{row.node.Informationen}}</v-card-subtitle>
           <v-card-text class="renn-infos">
-            <v-row v-if="row.node.Rennserie">
+            <v-row v-show="row.node.Rennserie">
               <v-col md="3">
                 <v-icon>mdi-car-multiple</v-icon>Rennserie:
               </v-col>
               <v-col md="9">{{row.node.Rennserie}}</v-col>
             </v-row>
-            <v-row v-if="row.node.Ort">
+            <v-row v-show="row.node.Ort">
               <v-col md="3">
                 <v-icon>mdi-map-marker-radius</v-icon>Austragungsort:
               </v-col>
               <v-col md="9">{{row.node.Ort}}</v-col>
             </v-row>
-            <v-row v-if="row.node.Rennstart">
+            <v-row v-show="row.node.Rennstart">
               <v-col md="3">
                 <v-icon>mdi-traffic-light</v-icon>Rennstart:
               </v-col>
               <v-col md="9">
-                <span v-if="toggleTimeLineDense">{{row.node.Datum}}</span>
+                <span v-show="toggleTimeLineDense">{{row.node.Datum}}</span>
                 {{row.node.Rennstart}}
               </v-col>
             </v-row>
-            <v-row v-if="row.node.TV_und_Livestream">
+            <v-row v-show="row.node.TV_und_Livestream">
               <v-col md="3">
                 <v-icon>mdi-youtube-tv</v-icon>Übertragung:
               </v-col>
               <v-col md="9">{{row.node.TV_und_Livestream}}</v-col>
             </v-row>
-            <v-row v-if="row.node.public_viewing">
+            <v-row v-show="row.node.public_viewing">
               <v-col md="3">
                 <v-icon>mdi-account-multiple</v-icon>Public viewing:
               </v-col>
